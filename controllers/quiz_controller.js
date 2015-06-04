99d96481ac2122adfff8ef2f8743ec91dbce1708
jcroa <jcarlos.roa@gmail.com>
2015-06-04 22:57:32 +0200

    Primera pregunta

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,19 @@
+// Qujiz Controller
+
+// GET /quizes/question
+exports.question = function(req, res) {
+   // public/quizes/question.ejs
+   res.render('quizes/question', {
+           pregunta: 'Capital de Italia'
+       });
+};
+
+// GET /quizes/answer
+exports.answer = function(req, res) {
+   // public/quizes/answer.ejs
+   if (req.query.respuesta === 'Roma'){
+      res.render('quizes/answer', {respuesta: 'Correcto'});
+   } else {
+      res.render('quizes/answer', {respuesta: 'Incorrecto'});
+   }
+};
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+///
+/// require módulos propios
+///
+
+// primera pregunta
+var quizController = require('../controllers/quiz_controller');
+
 /* GET home page. */
 router.get('/', function(req, res) {
   //  views/index.ejs  más parámetros.
   res.render('index', { title: 'Quiz demo' });
 });
+
+
+// primera pregunta
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer',   quizController.answer);
+
 
 module.exports = router;
