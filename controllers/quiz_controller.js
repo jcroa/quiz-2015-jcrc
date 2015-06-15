81a3dd87a7fcedccbe427b869f09f22eaa3d2b96
jcroa <jcarlos.roa@gmail.com>
2015-06-15 23:41:13 +0200

    código para sequelize. sqlite falla instalación

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,21 +1,29 @@
-// Qujiz Controller
+// Quiz Controller
+
+// Importación de los modelos de datos.
+var models = require("../models./models.js");
 
 // GET /quizes/question
 exports.question = function(req, res) {
-   // public/quizes/question.ejs
-   res.render('quizes/question', {
-           pregunta: 'Capital de Italia'
-       });
+    models.Quiz.findAll().success(function(rows) {
+        // Manejo de evento success de findAll()
+        var enunciado = rows[0].pregunta;
+        // public/quizes/question.ejs
+        res.render('quizes/question', { pregunta: enunciado });
+    });
 };
 
 // GET /quizes/answer
 exports.answer = function(req, res) {
-   // public/quizes/answer.ejs
-   if (req.query.respuesta === 'Roma'){
-      res.render('quizes/answer', {respuesta: 'Correcto'});
-   } else {
-      res.render('quizes/answer', {respuesta: 'Incorrecto'});
-   }
+    models.Quiz.findAll().success(function(rows) {
+        var resp = rows[0].respuesta;
+        // public/quizes/answer.ejs
+        if (req.query.respuesta === resp){
+           res.render('quizes/answer', {respuesta: 'Correcto'});
+        } else {
+           res.render('quizes/answer', {respuesta: 'Incorrecto'});
+        }
+    });
 };
 
 exports.author = function(req, res) {
---
+++ models/models.js
@@ -0,0 +1,37 @@
+///
+/// Definición de los modelos de datos
+///
+
+// Importación de módulos
+var path = require("path");
+var Sequelize = require("Sequelize"); // modelo ORM
+
+// Usar BBDD SqLite. Guardado en fichero quiz.sqlite
+var sequelize = new Sequelize(null, null, null,
+        {dialect: "sqlite", storage: "quiz.sqlite"}
+    );
+    
+// Imortar la definición de la tabla Quiz en quiz.js
+var Quiz = sequelize.import(path.join(__dirname, "quiz");
+
+exports.Quiz = Quiz;
+
+// Creamos e inicializamos tabla de preguntas en la base de datos
+sequelize.sync().success(function() {
+    // manejador de evento success de sync(...)
+    Quiz.count().success(function(count) {
+        // manejador de evento success de count(...)
+        if (count===0) {
+            // tabla está vacía: Añadimos datos de prueba
+            Quiz.create({
+                pregunta: "Capital de Italia",
+                respuesta: "Roma"
+            }).success(function() {
+               // manejador de evento success de create(...)
+               console.log("Base de datos inicializada con un registro");
+            });
+        }
+    });
+});
+
+  
