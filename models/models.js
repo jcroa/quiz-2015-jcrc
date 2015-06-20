aa167ed5f80d7252a43eeeabd077064e09c7cf30
jcroa <jcarlos.roa@gmail.com>
2015-06-20 17:07:43 +0200

    fixed: 'sequelize', no ·'Sequelize'.

--- models/models.js
+++ models/models.js
@@ -5,7 +5,6 @@
 // Importación de módulos
 var path = require("path");
 
-
 console.log("models.js - DATABASE_URL : " + process.env.DATABASE_URL);
 
 // Postgres DATABASE_URL = postgres://rkjqotjtqcmjmn:SnJ09qa_ABejE1i1SjMBiqU24n@ec2-54-83-43-118.compute-1.amazonaws.com:5432/d578grmpsb38gc
@@ -20,8 +19,19 @@ var port     = (url[5]||null);
 var host     = (url[4]||null);
 var storage  = process.env.DATABASE_STORAGE;
 
+var dbOptions = { 
+        dialect:  protocol,
+        protocol: protocol,
+        port:     port,
+        host:     host,
+        storage:  storage,  // solo SQLite (.env)
+        omitNull: true      // solo Postgres
+    };
+
+console.log("models.js - DbOptions: \n" + JSON.stringify(dbOptions));
+
 // Carga modelo ORM
-var Sequelize = require("Sequelize"); // modelo ORM
+var Sequelize = require("sequelize"); // modelo ORM
 
 // Usar BBDD SQLite o Postgres
 var sequelize = new Sequelize(DB_name, user, pwd, 
@@ -34,11 +44,6 @@ var sequelize = new Sequelize(DB_name, user, pwd,
         omitNull: true      // solo Postgres
     }      
 );
-
-// Usar BBDD SqLite. Guardado en fichero quiz.sqlite
-var sequelize = new Sequelize(null, null, null,
-        {dialect: "sqlite", storage: "quiz.sqlite"}
-    );
     
 // Importar la definición de la tabla Quiz en quiz.js
 var quizPath = path.join(__dirname, "quiz");
@@ -63,5 +68,7 @@ sequelize.sync().success(function() {
         }
     });
 });
+
+console.log("models.js - Init OK ");
 
   
